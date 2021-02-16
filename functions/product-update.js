9b165f9509919ad6a76628e22eb5ca583c07dfe4
mario <mario@uniplaces.com>
2021-02-16 12:47:48 +0000

    function update

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,5 +1,6 @@
 const faunadb = require('faunadb');
 const verifyWebhookIntegrity = require('shopify-verify-webhook');
+const axios = require('axios');
 
 const q = faunadb.query;
 const client = new faunadb.Client({
@@ -14,10 +15,51 @@ exports.handler = function (event, context, callback) {
   );
 
   if (isValid) {
-    callback(null, {
-      statusCode: 200,
-      body: 'Hello World!',
+    const body = JSON.parse(event.body);
+    const { id } = body;
+    delete body.updated_at;
+    body.variants.forEach(variant => {
+      delete variant.updated_at;
+      delete variant.inventory_quantity;
+      delete variant.old_inventory_quantity;
     });
+
+    const bodyString = JSON.stringify(body);
+
+    client
+      .query(q.Get(q.Match(q.Index('product_by_id', id))))
+      .then(result => {
+        if (result.data.prodcut !== bodyString) {
+          // rebuild
+          client
+            .query(
+              q.Update(result.ref, {
+                data: { product: bodyString },
+              })
+            )
+            .then(() => {
+              // rebuild
+              axios.post(process.env.NETLIFY_BUILD_URL);
+            })
+            .catch(e => {
+              console.log('error updating product: ', e);
+            });
+        }
+      })
+      .catch(
+        client.query(
+          q.Create(q.Collection('products'), {
+            data: { id, product: bodyString },
+          })
+        )
+      )
+      .then(() => {
+        //rebuild
+        axios.post(process.env.NETLIFY_BUILD_URL);
+      })
+      .catch(e => {
+        console.log('Error adding to db: ', e);
+      });
   } else {
     callback(null, {
       statusCode: 403,
