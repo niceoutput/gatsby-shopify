ecc1b73b31b9ddf0646874d8c538a6739edf9a9f
mario <mario@uniplaces.com>
2021-02-16 12:08:56 +0000

    add netlify funtion to update the endpoint

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,6 +1,27 @@
+const faunadb = require('faunadb');
+const verifyWebhookIntegrity = require('shopify-verify-webhook');
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET,
+});
+
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello World!',
-  });
+  const isValid = verifyWebhookIntegrity(
+    process.env.SHOPIFY_WEBHOOK_KEY,
+    event.headers['x-shopify-hmav-sha256'],
+    event.body
+  );
+
+  if (isValid) {
+    callback(null, {
+      statusCode: 200,
+      body: 'Hello World!',
+    });
+  } else {
+    callback(null, {
+      statusCode: 403,
+      body: 'Error!',
+    });
+  }
 };
